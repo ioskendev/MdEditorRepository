a21014ad27f447c59850fa6f66ac1ad715395f52
maksimaleksev <59773630+maksimaleksev@users.noreply.github.com>
2024-01-13 21:31:18 +0300

    added localization

--- Project/Resources/Autogenerated/Strings.swift
+++ Project/Resources/Autogenerated/Strings.swift
@@ -5,6 +5,37 @@ import Foundation
 // swiftlint:disable all
 
 internal enum L10n {
+
+  internal enum LoginScreen {
+    /// Authorization
+    internal static let title = L10n.tr("Localizable", "LoginScreen.Title")
+
+    internal enum Button {
+
+      internal enum Login {
+        /// Login
+        internal static let title = L10n.tr("Localizable", "LoginScreen.Button.Login.Title")
+      }
+    }
+
+    internal enum Textfield {
+
+      internal enum Login {
+        /// Login
+        internal static let placeholder = L10n.tr("Localizable", "LoginScreen.Textfield.Login.Placeholder")
+      }
+
+      internal enum Password {
+        /// Login
+        internal static let placeholder = L10n.tr("Localizable", "LoginScreen.Textfield.Password.Placeholder")
+      }
+    }
+  }
+
+  internal enum TodoListScreen {
+    /// ToDoList
+    internal static let title = L10n.tr("Localizable", "TodoListScreen.Title")
+  }
 }
 
 extension L10n {

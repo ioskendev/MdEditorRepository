e6d7a88ccd629d50e4971e81d5e862456b61b540
Dinar Khakimov <56588291+DinarKhakimov@users.noreply.github.com>
2024-01-15 22:57:47 +0500

    Added extensions for UIColor,  added Theme and applied it.

---
+++ Project/Sources/Extensions/UIColor+Hex.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Hex.swift
+//  MdEditor
+//
+//  Created by Dinar Khakimov on 15.01.2024.
+//
+
+import Foundation
---
+++ Project/Sources/Theme/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  MdEditor
+//
+//  Created by Dinar Khakimov on 15.01.2024.
+//
+
+import Foundation

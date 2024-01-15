274a07206d1dc8f361414c87ca69c7a5ff079606
Dinar Khakimov <56588291+DinarKhakimov@users.noreply.github.com>
2024-01-15 23:19:00 +0500

    Added extensions for UIColor, added Theme and applied it.

--- Project/Sources/Extensions/UIColor+Dynamic.swift
+++ Project/Sources/Extensions/UIColor+Dynamic.swift
@@ -2,7 +2,41 @@
 //  UIColor+Dynamic.swift
 //  MdEditor
 //
-//  Created by Dinar Khakimov on 15.01.2024.
+//  Created by ioskendev on 15.01.2024.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+	static func color(light: UIColor, dark: UIColor, highContrastLight: UIColor, highContrastDark: UIColor) -> UIColor {
+		if #available(iOS 13, *) {
+			return .init { traitCollection in
+				let isDarkMode = traitCollection.userInterfaceStyle == .dark
+				let isHighContrast = traitCollection.accessibilityContrast == .high
+
+				switch (isDarkMode, isHighContrast) {
+				case (true, false):
+					return dark
+				case (true, true):
+					return highContrastDark
+				case (false, false):
+					return light
+				case (false, true):
+					return highContrastLight
+				}
+			}
+		} else {
+			return light
+		}
+	}
+
+	static func color(light: UIColor, dark: UIColor) -> UIColor {
+		if #available(iOS 13, *) {
+			return .init { traitCollection in
+				return traitCollection.userInterfaceStyle == .dark ? dark : light
+			}
+		} else {
+			return light
+		}
+	}
+}
--- Project/Tests/UIColorHexTests.swift
+++ Project/Tests/UIColorHexTests.swift
@@ -2,40 +2,73 @@
 //  UIColorHexTests.swift
 //  MdEditorTests
 //
-//  Created by Dinar Khakimov on 15.01.2024.
+//  Created by ioskendev on 15.01.2024.
 //
 
 import XCTest
+@testable import MdEditor
+
+// swiftlint:disable force_cast
 
 final class UIColorHexTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_withValidParameters_shouldBeInitSuccess() {
+        let red: UInt8 = 0x26
+        let green: UInt8 = 0xea
+        let blue: UInt8 = 0x58
+        let alpha: UInt8 = 0xff
+        let expectedColor = UIColor(red: 0x26 / 255.0, green: 0xea / 255.0, blue: 0x58 / 255.0, alpha: 1)
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+        let sut = UIColor(red: red, green: green, blue: blue, alpha: alpha)
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCTAssertEqual(sut, expectedColor, "Ошибка создания цвета")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_init_withWrongInt_shouldBeInitFailure() {
+        let color = 0x987654321577
+        let expectedColor = UIColor(red: 0x98 / 255.0, green: 0x76 / 255.0, blue: 0x54 / 255.0, alpha: 0x32 / 255.0)
+
+        let sut = UIColor(hex: color)
+
+        XCTAssertNotEqual(sut, expectedColor, "Ошибка создания цвета")
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+    func test_init_withWrongInt_shouldBeInitSuccess() {
+        let color = 0x987654321577
+        let expectedColor = UIColor(red: 0x54 / 255.0, green: 0x32 / 255.0, blue: 0x15 / 255.0, alpha: 0x77 / 255.0)
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let sut = UIColor(hex: color)
+
+        XCTAssertEqual(sut, expectedColor, "Ошибка создания цвета")
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func test_init_withValidHexString_shouldBeInitSuccess() throws {
+        let color = "00ff00"
+        let expectedColor = UIColor(red: 0 / 255.0, green: 255 / 255.0, blue: 0 / 255.0, alpha: 1)
+
+        let sut = UIColor(hex: color)
+
+        XCTAssertEqual(sut, expectedColor, "Ошибка создания цвета")
+    }
+
+    func test_init_withValidHexString2_shouldBeInitSuccess() throws {
+        let color = "26ea58"
+        let expectedColor = UIColor(red: 38 / 255.0, green: 234 / 255.0, blue: 88 / 255.0, alpha: 1)
+
+        let sut = UIColor(hex: color)
+
+        XCTAssertEqual(sut, expectedColor, "Ошибка создания цвета")
+    }
+
+    func test_init_withWrongHexString_shouldBeInitFailure() throws {
+        let color = "26ww58"
+        let expectedColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+
+        let sut = UIColor(hex: color)
+
+        XCTAssertNotNil(sut)
+        XCTAssertEqual(sut, expectedColor, "Ошибка создания цвета")
     }
 }
+
+// swiftlint:enable force_cast

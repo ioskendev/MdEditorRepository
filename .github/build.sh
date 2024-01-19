5407479e925cb7b1abc28f5fce39364515ec5114
iosken <ioskendev@gmail.com>
2024-01-19 07:54:06 +0500

    ci returned to default

--- .github/build.sh
+++ .github/build.sh
@@ -1,6 +1,5 @@
-xcrun instruments -w 'iPhone 14 Pro Max (latest)' || sleep 15
-
-curl -Ls https://install.tuist.io | bash
+cd ./Project
+curl -Ls https: install.tuist.io | bash
 tuist install 3.36.2
 tuist fetch
 tuist generate
@@ -9,4 +8,4 @@ xcodebuild clean -quiet
 xcodebuild build-for-testing \
     -workspace 'MdEditor.xcworkspace' \
     -scheme 'MdEditor' \
-    -destination 'platform=iOS Simulator,name=iPhone 14 Pro Max,OS=latest'
+    -destination 'platform=iOS Simulator,name=iPhone 14 Pro Max'
--- .github/test.sh
+++ .github/test.sh
@@ -1,8 +1,6 @@
 cd ./Project
 
-xcrun instruments -w 'iPhone 14 Pro Max (latest)' || sleep 15
-
 xcodebuild test-without-building \
     -workspace 'MdEditor.xcworkspace' \
     -scheme 'MdEditor' \
-    -destination 'platform=iOS Simulator,name=iPhone 14 Pro Max,OS=latest'
+    -destination 'platform=iOS Simulator,name=iPhone 14 Pro Max'
